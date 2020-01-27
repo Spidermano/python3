852d7276efabbad0f1aa6bc5ef6babec3845518b
Spidermano <60321997+Spidermano@users.noreply.github.com>
2020-01-27 20:39:04 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import os
+from flask import jsonify
+import random as rnd
+app=Flask(__name__)
+
+@app.route('/')
+def bot():
+    return 'hello'
+
+@app.route('/api/<name>')
+def hey(name):
+    message=[
+        "hi,",
+        "wellcome, ",
+        "Fuck off, "
+        ]
+    return jsonify({
+    "messages": [
+    {"text": rnd.choice(message) + str(name)}
+    ]
+})
+
+    
+if __name__=="__main__":
+    app.debug=True
+    app.port=int(os.environ.get('PORT',5000))
+    app.run()
